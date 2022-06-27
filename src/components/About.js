4d7c5a67c3ed132009906cf42b867318e750c8a7
Kavin Chaisawangwong <kavinchai00@gmail.com>
2022-06-27 15:59:52 -0500

    Project card css applied

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import "../css/About.css";
 const About = () => {
   const [myName, setMyName] = useState(true);
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "../css/ProjectCard.css";
+import {
+  VscJson,
+  VscLinkExternal,
+  VscSymbolFile,
+  VscFileCode,
+} from "react-icons/vsc";
 
 const ProjectCard = ({ proj: { title, desc, tech } }) => {
   return (
     <a className="projectCardContainer">
-      <div className="projHeader">{title}</div>
-      <div className="projBody"></div>
-      <div className="projFooter"></div>
+      <div className="projHeaderContainer">
+        <div className="projHeader">
+          <VscFileCode className="projCardIcon1" />
+          <VscLinkExternal className="projCardIcon2" />
+        </div>
+      </div>
+      <div className="projBody">
+        <div className="projTitle">{title}</div>
+        <div className="projDesc">{desc}</div>
+      </div>
+      <div className="projFooter">
+        <div className="projTech">
+          {tech.map((key, index) => (
+            <div className={`techUsed techUsed${index + 1}`}>{key}</div>
+          ))}
+        </div>
+      </div>
     </a>
   );
 };
